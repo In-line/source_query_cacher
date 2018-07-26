4942fdc38232dd4ab504d355d5eea7891b11b62b
Alik Aslanyan <cplusplus256@gmail.com>
2018-07-27 00:06:41 +0400

    Snap

---
+++ src/bin/cache_updater.rs
@@ -0,0 +1,10 @@
+extern crate tokio_codec;
+
+use tokio_codec::Decoder;
+
+struct ChallengeReques {
+
+}
+
+impl Decoder for ChallengeRequest {
+}
---
+++ src/bin/main.rs
@@ -0,0 +1,7 @@
+use std::net::SocketAddr;
+use std::env;
+
+fn main() {
+    let addr = env::args().nth(1).unwrap_or("127.0.0.1:8080".to_string());
+    let addr = addr.parse::<SocketAddr>().unwrap();
+}
