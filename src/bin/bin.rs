c9a485ad89c78f983213feb6fbe942afcb6f1fa7
Alik Aslanyan <cplusplus256@gmail.com>
2018-11-08 00:08:52 +0400

    Update bin.rs

--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -18,13 +18,12 @@ extern crate futures;
 extern crate itertools;
 extern crate source_query_cacher;
 extern crate tokio;
-#[macro_use]
 extern crate structopt;
 
 use source_query_cacher::cacher;
 use std::net::SocketAddr;
 use std::time::Duration;
-use structopt::StructOpt;
+//use structopt::StructOpt;
 
 use itertools::*;
 use tokio::prelude::future::*;
